03cfb5a2cb1e51c26c2b0799ce5e2855999bad49
Filipe Ferreira <filipesferreira94@gmail.com>
2021-04-09 18:51:53 +0100

    Add the first Todo's to the main class

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args){
+        //TODO: System of authentication
+        // Design Pattern: Singleton
+
+        //TODO: save the daily production
+        // -> sectors; parks; regions; countries;
+        // Design Pattern:
+
+        //TODO: Calculate the rentability of energy by region.
+        // Design Pattern: Bridge
+
+        //TODO: Panels with the same caracteristics
+        // Design Pattern:
+
+        //TODO: Need save the data from the last 3 states of each pannel
+        // Design Pattern: Memento
+
+        //TODO: Each sector need have some more configurations
+        // Design Pattern:
+
+        //TODO: Design the UML file.
+        // Design Pattern:
+    }
+}
