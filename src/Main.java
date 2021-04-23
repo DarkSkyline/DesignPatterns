39db27ed57fdeeb8cee739596a31c7ef715be7e4
Filipe Ferreira <filipesferreira94@gmail.com>
2021-04-23 22:16:21 +0100

    Update the patterns

--- src/Main.java
+++ src/Main.java
@@ -22,19 +22,19 @@ public class Main {
 
         //TODO: save the daily production
         // -> sectors; parks; regions; countries;
-        // Design Pattern:
+        // Design Pattern: Composite
 
         //TODO: Calculate the rentability of energy by region.
         // Design Pattern: Bridge
 
         //TODO: Panels with the same characteristic
-        // Design Pattern: ObjectPool
+        // Design Pattern: Factory
 
         //TODO: Need save the data from the last 3 states of each pannel
         // Design Pattern: Memento
 
         //TODO: Each sector need have some more configurations
-        // Design Pattern:
+        // Design Pattern: Decorator
 
         //TODO: Design the UML file.
     }
